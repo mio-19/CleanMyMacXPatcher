547dea2f946cbbc81cbfa1b51153b1d62ed6c7dd
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-08-08 20:10:37 +0800

    implement the patcher

--- CleanMyMacXPatcher/CleanMyMacXPatcher.m
+++ CleanMyMacXPatcher/CleanMyMacXPatcher.m
@@ -6,7 +6,20 @@
 //
 
 #import "CleanMyMacXPatcher.h"
+#import <objc/runtime.h>
 
 @implementation CleanMyMacXPatcher
+
+-(BOOL)isAppActivated{
+    return true;
+}
+
++(void)load{
+    NSLog(@"-- Clean My Mac Patcher loading ...");
+    Method origMethod = class_getInstanceMethod(NSClassFromString(@"CMMacPawAccountActivationManager"), NSSelectorFromString(@"isAppActivated"));
+    Method newMethod = class_getInstanceMethod([CleanMyMacXPatcher class], @selector(isAppActivated));
+
+    method_exchangeImplementations(origMethod, newMethod);
+}
 
 @end
