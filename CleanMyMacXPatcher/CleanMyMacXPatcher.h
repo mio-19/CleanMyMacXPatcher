830052d444b8fcc18d6f6602a82da1c1edcdda82
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-08-08 20:05:07 +0800

    Initial Commit

---
+++ CleanMyMacXPatcher/CleanMyMacXPatcher.h
@@ -0,0 +1,12 @@
+//
+//  CleanMyMacXPatcher.h
+//  CleanMyMacXPatcher
+//
+//  Created by user on 8/8/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CleanMyMacXPatcher : NSObject
+
+@end
---
+++ CleanMyMacXPatcher/CleanMyMacXPatcher.m
@@ -0,0 +1,12 @@
+//
+//  CleanMyMacXPatcher.m
+//  CleanMyMacXPatcher
+//
+//  Created by user on 8/8/22.
+//
+
+#import "CleanMyMacXPatcher.h"
+
+@implementation CleanMyMacXPatcher
+
+@end
